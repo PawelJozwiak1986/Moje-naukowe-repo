c8b9c9407f8b37720e6709378a0788fec707cc84
Pawel <paweljozwiak1986@gmail.com>
2018-10-05 12:47:52 +0200

    Game done

--- Potfolio-landing/snake.js
+++ Potfolio-landing/snake.js
@@ -23,12 +23,15 @@ function drawSnake(){
   })
 
 }
+
 function moveSnake(){
   const head ={x: snake[0].x + moveX, y: snake[0].y + moveY}
   snake.unshift(head);
-  snake.pop();
-
-}
+  if(snake[0].x === appleX && snake[0].y === appleY)
+    createApple ()
+  else snake.pop();
+  
+  }
 function snakeSteer(event){
   const key = event.keyCode;
   if(key === 37 && moveX !== 10){
@@ -57,25 +60,42 @@ function drawApple(){
 function createApple(){
   appleX = randomApple(0, canvas.width - 10);
   appleY = randomApple(0, canvas.height - 10);
+  snake.forEach(function isColliding(snakePart) {
+    if (snakePart = snakePart.x === appleX && snakePart.y === appleY) createApple();
+    })
 
 }
 function randomApple(min, max){
   return Math.round((Math.random() * (max - min) + min) / 10) * 10;
 
 }
+function gameOver() {
+  for (let i = 4; i < snake.length; i++) {
+  if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) return true;
+  }
+    if(
+      snake[0].x < 0 ||
+      snake[0].y < 0 ||
+      snake[0].x > canvas.width - 10 ||
+      snake[0].y > canvas.height - 10 
+      
+    )
+    return true
+  }
 
 window.addEventListener('keydown', snakeSteer);
 
 createApple();
 
 function render(){
+  if (gameOver()) return;
 setTimeout(function(){
   drawCanvas();
   drawApple();
   moveSnake();
   drawSnake();
   render();
-},300)
+},200)
 
 }
 render();
