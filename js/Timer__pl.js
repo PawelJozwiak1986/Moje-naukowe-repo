bb7d17465ff936ae124367a95db37a51d24f724b
Pawel <paweljozwiak1986@gmail.com>
2018-11-28 13:30:34 +0100

    fixes to index

---
+++ js/Timer__pl.js
@@ -0,0 +1,10 @@
+function formatAMPM() {
+  var d = new Date(),
+  seconds = d.getSeconds().toString().length == 1 ? '0' + d.getSeconds() : d.getSeconds(),
+  minutes = d.getMinutes().toString().length == 1 ? '0' + d.getMinutes() : d.getMinutes(),
+  hours = d.getHours().toString().length == 1 ? '0' + d.getHours() : d.getHours(),
+  months = ['Sty', 'Lut', 'Ma', 'Kwie', 'Maj', 'Czerw', 'Lip', 'Sie', 'Wrze', 'Paź', 'List', 'Gru'],
+  days = ['Pon', 'Wt', 'Śr','Czw', 'Pt', 'Sob', 'Niedz'];
+  document.getElementById("para1").innerHTML = days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ':' + seconds;
+  }
+  setInterval(formatAMPM, 1000);
