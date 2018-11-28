46e8a5f11cf9ec8bd7d235ae0eadcb91f2438fc2
Pawel <paweljozwiak1986@gmail.com>
2018-11-28 12:35:46 +0100

    eng description to index__eng

--- js/Timer.js
+++ js/Timer.js
@@ -3,9 +3,8 @@ function formatAMPM() {
     seconds = d.getSeconds().toString().length == 1 ? '0' + d.getSeconds() : d.getSeconds(),
     minutes = d.getMinutes().toString().length == 1 ? '0' + d.getMinutes() : d.getMinutes(),
     hours = d.getHours().toString().length == 1 ? '0' + d.getHours() : d.getHours(),
-    ampm = d.getHours() >= 12 ? 'pm' : 'am',
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    document.getElementById("para1").innerHTML = days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+    document.getElementById("para1").innerHTML = days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ':' + seconds;
     }
     setInterval(formatAMPM, 1000);
